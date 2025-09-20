7972c9ea49680835938c97ccd8e6143b4570d01e
Jin <hurjin1109@naver.com>
2025-09-20 16:00:19 +0900

    FEAT : Mongo Template 관련 설정 추가 - ignore -> application-mongo.yml 추가

---
+++ src/main/java/com/HeoJin/RecipeSearchEngine/config/mongo/MongoConfig.java
@@ -0,0 +1,43 @@
+package com.HeoJin.RecipeSearchEngine.config.mongo;
+
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
+
+@Configuration
+public class MongoConfig {
+
+    @Value("${spring.data.mongo.uri")
+    private String mongoUri;
+
+    @Value("${spring.data.mongo.database")
+    private String databaseName;
+
+
+    @Bean
+    public MongoClient mongoClient() {
+        return MongoClients.create(mongoUri);
+    }
+
+    @Bean
+    public MongoDatabaseFactory mongoDatabaseFactory() {
+        return new SimpleMongoClientDatabaseFactory(mongoClient(), databaseName);
+    }
+
+    @Bean
+    public MongoTemplate mongoTemplate(MongoDatabaseFactory mongoDatabaseFactory) {
+        return new MongoTemplate(mongoDatabaseFactory);
+    }
+
+
+
+
+
+
+}
