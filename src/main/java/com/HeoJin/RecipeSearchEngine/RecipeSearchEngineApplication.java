3289d0e1c5db0990436d54a454caf7c287ae533f
Jin <hurjin1109@naver.com>
2025-09-20 15:40:03 +0900

    FEAT : spring 초기 설정

---
+++ src/main/java/com/HeoJin/RecipeSearchEngine/RecipeSearchEngineApplication.java
@@ -0,0 +1,13 @@
+package com.HeoJin.RecipeSearchEngine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeSearchEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeSearchEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/HeoJin/RecipeSearchEngine/RecipeSearchEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HeoJin.RecipeSearchEngine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeSearchEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
