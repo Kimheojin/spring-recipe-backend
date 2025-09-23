19eba78e79dae7a35fd5b7b7318cae649e98a868
Jin <hurjin1109@naver.com>
2025-09-23 15:21:45 +0900

    FEAT : Dto 코드 초안 추가

---
+++ src/main/java/com/HeoJin/RecipeSearchEngine/IntegratedSearch/dto/ListSearchCookingOrderDto.java
@@ -0,0 +1,6 @@
+package com.HeoJin.RecipeSearchEngine.IntegratedSearch.dto;
+
+import java.util.List;
+
+public record ListSearchCookingOrderDto(List<SearchCookingOrderDto> searchCookingOrderDtoList) {
+}
---
+++ src/main/java/com/HeoJin/RecipeSearchEngine/IntegratedSearch/dto/ListSearchIngredientDto.java
@@ -0,0 +1,6 @@
+package com.HeoJin.RecipeSearchEngine.IntegratedSearch.dto;
+
+import java.util.List;
+
+public record ListSearchIngredientDto(List<SearchIngredientDto> searchIngredientDtoList) {
+}
