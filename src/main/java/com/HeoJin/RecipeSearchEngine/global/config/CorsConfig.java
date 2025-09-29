833647d3e1a868c323aa1df7502bbe4dba6bf360
Jin <hurjin1109@naver.com>
2025-09-29 17:00:06 +0900

    FIX : CorsConfiguration -> mvc 방식으로 cors 수정 - spring Security 사용 안할 경우 적용 안되는 문제

--- src/main/java/com/HeoJin/RecipeSearchEngine/global/config/CorsConfig.java
+++ src/main/java/com/HeoJin/RecipeSearchEngine/global/config/CorsConfig.java
@@ -1,35 +1,21 @@
 package com.HeoJin.RecipeSearchEngine.global.config;
 
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
-import java.util.List;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
+public class CorsConfig implements WebMvcConfigurer {
 
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.setAllowCredentials(true);
-
-        corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:1000", "http://localhost:5173"));
-        corsConfiguration.setAllowedHeaders(List.of("*")); // 고정값
-        corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-
-        // preflight 한시간 캐시
-        corsConfiguration.setMaxAge(3600L);
-        source.registerCorsConfiguration("/seo/**", corsConfiguration);
-
-        return source;
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/seo/**")
+                .allowCredentials(true)
+                .allowedOrigins("http://localhost:1000", "http://localhost:5173")
+                .allowedHeaders("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .maxAge(3600);
     }
-
-
 }
 
