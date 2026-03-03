9b1542bc675d35ef0a6306806fdbbc295d651eee
Jin <hurjin1109@naver.com>
2026-03-03 20:53:52 +0900

    REFACTORE : 파이프라인 쿼리 표현 방식 변경

--- src/main/java/com/HeoJin/RecipeSearchEngine/autocomplete/repository/AutocompleteRepositoryImpl.java
+++ src/main/java/com/HeoJin/RecipeSearchEngine/autocomplete/repository/AutocompleteRepositoryImpl.java
@@ -28,101 +28,150 @@ public class AutocompleteRepositoryImpl implements AutocompleteRepository {
     // 재료명 기반 자동 완성
     @Override
     public List<AutocompleteIngredientDto> getResultAboutIngredient(String term) {
-
-        // 인덱스 지정 json
-        Document ingredientSearchStage = new Document("$search",
-                new Document("index", "autocomplete_kr")
-                        .append("autocomplete", new Document("query", term)
-                                .append("path", "ingredientList")
-                                .append("tokenOrder", "any")));
-
-        // score json 필드 추가
-        Document addFieldsStage = new Document("$addFields",
-                new Document("score", new Document("$meta", "searchScore")));
-
         Aggregation aggregation = Aggregation.newAggregation(
-                Aggregation.stage(ingredientSearchStage),
-                Aggregation.stage(addFieldsStage),
-                // ingredientList 중 term 으로 시작하는 값만 필터링
+                // search 연산자
+                // 문서 단위 검색
+                Aggregation.stage(Document.parse("""
+                        {
+                          "$search": {
+                            "index": "autocomplete_kr",
+                            "autocomplete": {
+                              "query": "%s",
+                              "path": "ingredientList",
+                              "tokenOrder": "any"
+                            }
+                          }
+                        }
+                        """.formatted(term))),
+
+                // Score 필드 추가 파이프
+                // Document 에 붙음
+                Aggregation.stage(Document.parse("""
+                        {
+                          "$addFields": { "score": { "$meta": "searchScore" } }
+                        }
+                        """)),
+
+                // 요소 단위 추출
+                // matchingIngredients 배열 + score 필드 (아직 배열 형태)
                 Aggregation.project()
-                        .andExpression(filterIngredientExpression(term))
+                        .andExpression("""
+                                {
+                                    "$filter": {
+                                        "input": "$ingredientList",
+                                        "cond": {
+                                            "$regexMatch": {
+                                                "input": "$$this",
+                                                "regex": "^%s"
+                                            }
+                                        }
+                                    }
+                                }
+                                """.formatted(term))
                         .as("matchingIngredients")
                         .and("score").as("score"),
-                // 재료 배열 펼치기
+
+                // 배열 품
                 Aggregation.unwind("matchingIngredients"),
+                // score 는 필드 별로
                 Aggregation.project()
-                        .andExpression("$matchingIngredients")
-                        .as("ingredient")
+                        .andExpression("$matchingIngredients").as("ingredient")
                         .and("score").as("score"),
-                // 동일 재료명 중 가장 높은 score 선택
+                // 중복 데이터 묶고, score max 값 지정
                 Aggregation.group("ingredient")
                         .max("score").as("score"),
+
+                // 필드 관련 연산자
                 Aggregation.project()
+                        // 재료명에 담긴 _id 필드를 가져와 ingredient 필드 이름 할당
+                        // { "_id" : "사과" } -> { "ingredient" : "사과" }
                         .andExpression("$_id").as("ingredient")
-                        // score 기준 내림차순
                         .and("score").as("score"),
                 Aggregation.sort(Sort.by(Sort.Direction.DESC, "score")),
                 Aggregation.limit(10)
         );
 
-        AggregationResults<AutocompleteIngredientDto> results =
-                mongoTemplate.aggregate(aggregation, collectionName, AutocompleteIngredientDto.class);
-
-        return results.getMappedResults();
+        return mongoTemplate.aggregate(aggregation, collectionName, AutocompleteIngredientDto.class).getMappedResults();
     }
 
     // 레시피명 기반 자동 완성
     @Override
     public List<AutocompleteRecipeNameDto> getResultAboutRecipeName(String term) {
-
-        // highligth 설정
-        Document recipeSearchStage = new Document("$search",
-                new Document("index", "autocomplete_kr")
-                        .append("autocomplete", new Document("query", term)
-                                .append("path", "recipeName")
-                                .append("tokenOrder", "any"))
-                        .append("highlight", new Document("path", "recipeName")));
-        // score json
-        Document addFieldsStage = new Document("$addFields",
-                new Document("score", new Document("$meta", "searchScore")));
-        // highlight + score 만 projection
-        Document highlightsProjectStage = new Document("$project",
-                new Document("highlights", new Document("$meta", "searchHighlights"))
-                        .append("score", 1));
-
-        Document filterHits = new Document("$filter",
-                new Document("input", "$$this.texts")
-                        .append("cond", new Document("$eq", List.of("$$this.type", "hit"))));
-
-        Document innerReduce = new Document("$reduce",
-                new Document("input", "$highlights")
-                        .append("initialValue", List.of())
-                        .append("in", new Document("$concatArrays", List.of("$$value", filterHits))));
-
-        Document matchedTextExpression = new Document("$reduce",
-                new Document("input", innerReduce)
-                        .append("initialValue", "")
-                        .append("in", new Document("$concat", List.of("$$value", "$$this.value"))));
-
-        Document matchedTextStage = new Document("$addFields",
-                new Document("matchedText", matchedTextExpression));
-        // prefix 보장
-        Document matchedTextMatchStage = new Document("$match",
-                new Document("matchedText",
-                        new Document("$regex", "^%s".formatted(term))
-                                .append("$options", "i")));
-
         Aggregation aggregation = Aggregation.newAggregation(
-                Aggregation.stage(recipeSearchStage),
-                Aggregation.stage(addFieldsStage),
-                Aggregation.stage(highlightsProjectStage),
-                Aggregation.stage(matchedTextStage),
-                Aggregation.stage(matchedTextMatchStage),
+                // search 연산자 + 하이라이트 옵션 활성화
+                // 이건 전용 메서드가 아직 없어서 이렇게 해야됨
+                Aggregation.stage(Document.parse("""
+                        {
+                          "$search": {
+                            "index": "autocomplete_kr",
+                            "autocomplete": {
+                              "query": "%s",
+                              "path": "recipeName",
+                              "tokenOrder": "any"
+                            },
+                            "highlight": { "path": "recipeName" }
+                          }
+                        }
+                        """.formatted(term))),
+
+                // Document 에 메타데이터를 붙임
+                Aggregation.stage(Document.parse("{ '$addFields': { 'score': { '$meta': 'searchScore' } } }")),
+                // searchHighlights -> 이중 배열
+                Aggregation.stage(Document.parse("{ '$project': { 'highlights': { '$meta': 'searchHighlights' }, 'score': 1 } }")),
+
+                // reduce 연산자 -> 거의 함수
+                // input -> 원본 파라미터
+                // initValue -> 초깃값
+                // in -> 함수 본문
+                //$$this -> 현재 요소
+                // $$ value -> 누적 변수 (그냥 지역변수 ㅡㄴ낌)
+                Aggregation.stage(Document.parse("""
+                        {
+                          "$addFields": {
+                            "matchedText": {
+                              "$reduce": {
+                                "input": {
+                                  "$reduce": {
+                                    "input": "$highlights",
+                                    "initialValue": [],
+                                    "in": {
+                                      "$concatArrays": [
+                                        "$$value",
+                                        {
+                                          "$filter": {
+                                            "input": "$$this.texts",
+                                            "cond": { "$eq": ["$$this.type", "hit"] }
+                                          }
+                                        }
+                                      ]
+                                    }
+                                  }
+                                },
+                                "initialValue": "",
+                                "in": { "$concat": ["$$value", "$$this.value"] }
+                              }
+                            }
+                          }
+                        }
+                        """)),
+
+                // regex
+                Aggregation.stage(Document.parse("""
+                        {
+                          "$match": {
+                            "matchedText": { "$regex": "^%s", "$options": "i" }
+                          }
+                        }
+                        """.formatted(term))),
+
+
                 Aggregation.project()
                         .andExpression("$matchedText").as("recipeName")
                         .and("score").as("score"),
                 Aggregation.group("recipeName")
                         .max("score").as("score"),
+
+
                 Aggregation.project()
                         .andExpression("$_id").as("recipeName")
                         .and("score").as("score"),
@@ -130,25 +179,7 @@ public class AutocompleteRepositoryImpl implements AutocompleteRepository {
                 Aggregation.limit(10)
         );
 
-        AggregationResults<AutocompleteRecipeNameDto> results
-                = mongoTemplate.aggregate(aggregation, collectionName, AutocompleteRecipeNameDto.class);
-
-        return results.getMappedResults();
-    }
-
-    private String filterIngredientExpression(String term) {
-        return """
-                {
-                    "$filter": {
-                        "input": "$ingredientList",
-                        "cond": {
-                            "$regexMatch": {
-                                "input": "$$this",
-                                "regex": "^%s"
-                            }
-                        }
-                    }
-                }
-                """.formatted(term);
+        return mongoTemplate.aggregate(aggregation, collectionName, AutocompleteRecipeNameDto.class).getMappedResults();
     }
 }
+
